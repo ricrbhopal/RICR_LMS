a935e13433424c35b0fbd0a784eb59b582d71bca
Raj Vardhan <errvmaurya563@gmail.com>
2025-11-21 00:53:36 +0530

    feat: implement Navbar component with logo and StudentHeader integration

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.png";
+import StudentHeader from "./StudentHeader";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white border-b border-gray-200">
+        <div className="h-14 flex items-center justify-between w-full px-10">
+          <div className="flex items-center">
+            <img src={logo} alt="RICR LMS" className="h-6 w-auto" />
+            <span className="text-xl font-bold leading-none">RICR</span>
+          </div>
+          <StudentHeader  />
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
