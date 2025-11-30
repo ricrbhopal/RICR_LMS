885c7064706cfe40b6be853103e626e3a65e7aab
Raj Vardhan <errvmaurya563@gmail.com>
2025-11-30 15:24:21 +0530

    refactor: rename Navbar component file and update import in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
+
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import Home from "./pages/HomePage";
 import { ReferEarnProvider } from "./contexts/ReferEarnContext.jsx";
+
 
 function App() {
   const [active, setActive] = React.useState("Dashboard");
